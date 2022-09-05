86a3bee63408e12fbd2ecc7ac68f5a0f407cfdd4
Laimonade <leo.aimone94500@gmail.com>
2022-09-05 23:32:48 +0100

    Delete wrong master repo files

--- expectedError.m
+++
@@ -1,9 +0,0 @@
-function error = expectedError (evalFreq, sampleRate, blockSize, zeropad, pitchShiftRatio)
-    fa = evalFreq;
-    fs = sampleRate;
-    N = blockSize;
-    m = zeropad;
-    k = pitchShiftRatio;
-
-    error = ( ((fa + (fs / (2 * m * N))) * pitchShiftRatio) + (fs / (2 * m * N)) ) / (k * fa);
-end
